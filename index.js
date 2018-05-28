6a461cd1c381f5e56087c7283b060c5757650caa
Kishan Patel <kumarpatel25@gmail.com>
2018-05-28 13:24:45 -0400

    add stat block

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-var firstName;
-var lastName;
+var player = new Object();
+player.firstName;
+player.lastName;
 
 $("form").submit(function(e){
     e.preventDefault();
-    firstName = $("#firstNameInput").val();
-    lastName = $("#lastNameInput").val();
+    player.firstName = $("#firstNameInput").val();
+    player.lastName = $("#lastNameInput").val();
 
-    $("img").attr("src", "https://nba-players.herokuapp.com/players/"+lastName+"/"+firstName);
-}); 
+    $("img").attr("src", "https://nba-players.herokuapp.com/players/"+player.lastName+"/"+player.firstName);
+
+    $.get("https://nba-players.herokuapp.com/players-stats/"+player.lastName+"/"+player.firstName, function(data){
+        $("h2").text(player.firstName+" "+player.lastName+" - "+data.team_name);
+        $("#points").text("PPG: "+data.points_per_game);
+        $("#assts").text("APG: "+data.assists_per_game);
+        $("#rebs").text("RPG: "+data.rebounds_per_game);
+        $("#mins").text("MPG: "+data.minutes_per_game);
+    }, "json");
+});
